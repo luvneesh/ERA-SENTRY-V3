78f6c94833efd3551ae9447c56bd3bb2a4151cc7
Luvneesh Kumar <luvneeshkumar@Luvneeshs-MacBook-Pro.local>
2020-04-07 09:08:35 +0530

    problem ho gayi

--- vision_marker.py
+++ vision_marker.py
@@ -121,8 +121,9 @@ mybits = np.array([[0,0,0,0,0],[0,1,1,1,0],[0,1,1,1,0],[0,1,1,1,0],[0,0,0,0,0]],
 aruco_dict.bytesList[42] = aruco.Dictionary_getByteListFromBits(mybits)
 mybits = np.array([[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1]], dtype = np.uint8)
 aruco_dict.bytesList[43] = aruco.Dictionary_getByteListFromBits(mybits)
-
-frame = cv2.imread("lol3.png")
+# cap = cv2.VideoCapture('video/Sentry_1.mkv')
+# ret, frame = cap.read()
+frame = cv2.imread("lol5.png")
 
 frame1=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 lower_yel = np.array([23,41,133], dtype = "uint8")
@@ -133,8 +134,8 @@ frame1=255-frame1
 # frame1 = clahe.apply(frame1)
 
 # frame1 = cv2.adaptiveThreshold(frame1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-# cv2.imshow('1', frame1)
-# cv2.waitKey()
+cv2.imshow('1', frame1)
+# cv2.waitKey(-1)
 #lists of ids and the corners beloning to each id
 corners, ids, rejectedImgPoints = aruco.detectMarkers(frame1, aruco_dict)
 # draw markers on farme
